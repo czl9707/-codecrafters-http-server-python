9db1420e5411d6ec0cfed078a1d04b72c0b0274c
chenZL <czl970721@gmail.com>
2024-03-09 16:46:23 -0500

    listen and response 200

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    while True:
+        sock, response_addr = server_socket.accept()  # wait for client
+        request_handler(sock, response_addr)
+
+
+def request_handler(sock: socket.socket, response_addr) -> None:
+    request = sock.recv(1024)
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    sock.send(response)
+    sock.close()
 
 
 if __name__ == "__main__":
